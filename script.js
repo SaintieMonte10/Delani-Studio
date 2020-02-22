f62779b2219aef7cb27ffb4a914f334c844921b8
SaintieMonte10 <claremonte53@gmai.com>
2020-02-22 21:30:28 +0300

    added links to js and jquery

---
+++ script.js
@@ -0,0 +1,32 @@
+// // $(document).ready(function() {
+//     //Design toggle
+//     $(".design").click(function() {
+//         $(".design-par").show();
+//         $(this).hide();
+//     })
+//     $(".design-par").click(function() {
+//         $(".design").show();
+//         $(this).hide();
+//     })
+
+//     //Development toggle
+//     $(".development").click(function() {
+//         $(".development-par").show();
+//         $(this).hide();
+//     })
+//     $(".development-par").click(function() {
+//             $(".development").show();
+//             $(this).hide();
+//         })
+//         // product management
+//     $(".product").click(function() {
+//         $(".product-par").show();
+//         $(this).hide();
+//     })
+//     $(".product-par").click(function() {
+//         $(".product").show();
+//         $(this).hide();
+//     // })
+
+
+$
